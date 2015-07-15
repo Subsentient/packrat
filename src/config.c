a9bf2ffb79de2177cf3224a1a46cca3046fa2828
Subsentient <thinkingrodent@gmail.com>
2015-07-15 02:19:53 -0700

    More beginning work

---
+++ src/config.c
@@ -0,0 +1,120 @@
+/*Packrat package manager, Copyright 2015 (C) Subsentient
+
+This file is part of Packrat.
+
+Packrat is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Packrat is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Packrat.  If not, see <http://www.gnu.org/licenses/>.*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include "packrat.h"
+#include "substrings/substrings.h"
+
+
+//Globals
+char Arch[8][64];
+
+//Static function prototypes
+static bool Config_ProcessConfig(const char *ConfigStream);
+static bool Config_AddArch(const char *NewArch);
+static bool Config_ArchPresent(const char *CheckArch);
+
+//Actual functions
+bool Config_LoadConfig(void)
+{
+	struct stat FileStat;
+	
+	if (stat(CONFIGFILE_PATH, &FileStat) != 0)
+	{
+		return false;
+	}
+	
+	FILE *Descriptor = fopen(CONFIGFILE_PATH, "rb");
+	
+	if (!Descriptor) return false;
+	
+	char *ConfigStream = calloc(FileStat.st_size + 1, 1);
+	
+	fread(ConfigStream, 1, FileStat.st_size, Descriptor);
+	ConfigStream[FileStat.st_size] = '\0';
+	
+	fclose(Descriptor);
+	
+	Config_ProcessConfig(ConfigStream);
+	
+	free(ConfigStream);
+	return true;
+}
+
+static bool Config_ProcessConfig(const char *const ConfigStream)
+{
+	char CurrentLine[4096];
+	const char *Worker = ConfigStream;
+	unsigned LineNum = 1;
+	
+	char LineID[256];
+	char LineData[4096];
+	
+	for (; SubStrings.Line.GetLine(CurrentLine, sizeof CurrentLine, &Worker); ++LineNum)
+	{
+		const char *Ptr = CurrentLine;
+		
+		//Get line ID and advance past the delimiters.
+		SubStrings.CopyUntilC(LineID, sizeof LineID, &Ptr, "\t =", true);
+		
+		//Get line data.
+		SubStrings.Copy(LineData, Ptr, sizeof LineData);
+		
+		if (!strcmp(LineID, "Arch"))
+		{
+			if (!Config_AddArch(LineData))
+			{
+				fprintf(stderr, "No room for new arch %s\n", LineData);
+				return false;
+			}
+		}
+	}
+	
+	return true;
+}
+	
+static bool Config_AddArch(const char *NewArch)
+{
+	int Inc = 0;
+	const unsigned Max = (sizeof Arch / sizeof *Arch) - 1;
+	
+	for (; *Arch[Inc] != '\0' && Inc < Max; ++Inc);
+	
+	if (Inc == Max) return false;
+	
+	SubStrings.Copy(Arch[Inc], NewArch, sizeof Arch[Inc]);
+	
+	return true;
+}
+
+static bool Config_ArchPresent(const char *CheckArch)
+{
+	int Inc = 0;
+	const unsigned Max = (sizeof Arch / sizeof *Arch) - 1;
+
+	for (; *Arch[Inc] != '\0' && Inc < Max; ++Inc)
+	{
+		if (!strcmp(Arch[Inc], CheckArch)) return true;
+	}
+	
+	return false;
+}
+
---
+++ src/packrat.h
@@ -0,0 +1,27 @@
+/*Packrat package manager, Copyright 2015 (C) Subsentient
+
+This file is part of Packrat.
+
+Packrat is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Packrat is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Packrat.  If not, see <http://www.gnu.org/licenses/>.*/
+
+#ifndef _PACKRAT_H_
+#define _PACKRAT_H_
+
+#include <stdbool.h>
+#define CONFIGFILE_PATH "/etc/packrat.conf"
+
+
+//Globals
+extern char Arch[8][64];
+#endif //_PACKRAT_H_
